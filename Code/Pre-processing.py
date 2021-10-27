7261dc945287845743282c6f095493ddcb19073b
shrav15 <67496971+shrav15@users.noreply.github.com>
2021-10-27 20:12:09 +0530

    Sorted Data with SVM function
    Data is sorted properly depending on the low pass, high pass, normal and threshold. There are two function added, svm_extraction and get_train_test.

--- Code/Pre-processing.py
+++ Code/Pre-processing.py
@@ -4,7 +4,9 @@ from matplotlib import pyplot as plt
 import matplotlib
 import random
 import os
+from sklearn import svm
 
+from sklearn.model_selection import train_test_split
 """
 TODO:
 Add code that removes blank rows/columns from the input image. I will probably then add the same number of rows/columns at the bottom/right side
@@ -15,7 +17,6 @@ so that it doesn't break all of the other code.
 def extract_samples(img, imsize, spots):
     """
     Extracts samples from an image and returns them. 
-
     Parameters
     ----------
     img : numpy.ndarray (2D)
@@ -45,14 +46,12 @@ def extract_samples(img, imsize, spots):
 def print_samples(title, sample_list):
     """
         Prints all of the samples available in an array of samples. 
-
         Parameters
         ----------
         title : string
             The title for each image. A number will be appended to it. 
         sample_list : list, containing 20 elements. 
             A list of 20 image samples. 
-
         Returns
         -------
         None.  
@@ -86,7 +85,6 @@ def print_samples(title, sample_list):
 def get_samples(sr_no, dict_sample, source):
     """
         Takes a set of random samples from an image, after applying multiple filters.  
-
         Parameters
         ----------
         sr_no : int
@@ -96,17 +94,18 @@ def get_samples(sr_no, dict_sample, source):
             Contains all the data for a particular filter of the images.
         source: string
             Folder inside which the images are stored. 
-
         Returns
         -------
         None.  
     """
     
     os.chdir(source)
+    list_samples = []
     for f in os.listdir():
         img = cv2.imread(f,0)
         file,_ = os.path.splitext(f)
         file_name = file[:3]
+        list_samples.append(int(file_name))
         scale = 40
         width = int(img.shape[1] * scale / 100)
         height = int(img.shape[0] * scale / 100)
@@ -141,20 +140,110 @@ def get_samples(sr_no, dict_sample, source):
         dict_sample[sr_no][file_name]['highpass'] = highpass_img
         dict_sample[sr_no][file_name]['threshold']= threshed_img
     
-    
-    return dict_sample
+    return dict_sample,list_samples
 
 
 
 
-source = 'C:/Users/lenovo/Desktop/Educate/FYP/firemaker/300dpi/p1-copy-normal/'
-dict_sample = {}
-sr_no = '01'
-dict_sample[sr_no] = {}
-get_samples(sr_no, dict_sample, source)
+def get_train_test (type_data):
 
+    # As for now only working for P1 normal condtion 
+    source = 'D:/documents__/7th sem/fyp/firemaker/300dpi/p1-copy-normal/'
+    #source = 'C:/Users/lenovo/Desktop/Educate/FYP/firemaker/300dpi/p1-copy-normal/'
+    dict_sample = {}
+    sr_no = '01'
+    dict_sample[sr_no] = {}
+    get_samples(sr_no, dict_sample, source)
+    lowpass_data=[]
+    y_lowpass=[]
+    highpass_data=[]
+    y_highpass=[]
+    sample_data=[]
+    y_sample=[]
+    threshold_data=[]
+    y_threshold =[]
+    for i in dict_sample:
+        for j in dict_sample[i]:
+            for k in dict_sample[i][j]:
+                if k =='lowpass':
+                    sum_sample = dict_sample[i][j][k]
+                    for x in sum_sample:
+                        lowpass_data.append(x.flatten())
+                        y_lowpass.append(int(j))
+
+
+
+                elif k == 'highpass':
+                    sum_sample = dict_sample[i][j][k]
+                    for x in sum_sample:
+                        highpass_data.append(x.flatten())
+                        y_highpass.append(int(j))
+
+                elif k == 'sample':
+                    sum_sample = dict_sample[i][j][k]
+                    for x in sum_sample:
+                        sample_data.append(x.flatten())
+                        y_sample.append(int(j))
+                else :
+                    sum_sample = dict_sample[i][j][k]
+                    for x in sum_sample:
+                        threshold_data.append(x.flatten())
+                        y_threshold.append(int(j))
+
+    if type_data == 'lowpass':
+        # Covert list to array 
+        Y = np.array(y_lowpass)
+        X = np.array(lowpass_data)
+    elif type_data == 'highpass':
+        # Covert list to array 
+        Y = np.array(y_highpass)
+        X = np.array(highpass_data)
+    elif type_data == 'sample':
+        # Covert list to array 
+        Y = np.array(y_sample)
+        X = np.array(sample_data)
+    else :
+        # Covert list to array 
+        Y = np.array(y_threshold)
+        X = np.array(threshold_data)
+    return X,Y
+
+
+
+
+
+
+
+def Svm_extraction (X_train,Y_train,X_test,Y_test):
+
+
+   
+    model = svm.SVC(C=1,kernel = 'poly',gamma = 'auto')
+    model.fit(X_train,Y_train)
+    # Prediction 
+    prediction = model.predict(X_test)
+ 
+    # Score 
+    acc = model.score(X_test,Y_test)
+    print ('accuracy is :',acc)
+ 
+
+
+
+
+
+
+
+
+X,Y = get_train_test('lowpass')
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.8, random_state = 0)
+print (X.shape,Y.shape)
+Svm_extraction(X_train,Y_train,X_test,Y_test)
+
+"""
 print_samples('Lowpass', dict_sample[sr_no]['153']['lowpass'])
 print_samples('Highpass', dict_sample[sr_no]['153']['highpass'])
 print_samples('Threshold', dict_sample[sr_no]['153']['threshold'])
 print_samples('Samples', dict_sample[sr_no]['153']['sample'])
 plt.show()
+"""
